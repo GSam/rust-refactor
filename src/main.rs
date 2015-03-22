ec13ad9f85293fe51bb5408f86ff20208bfb76ff
Garming <garming_sam@outlook.com>
2015-03-23 01:12:36 +1300

    Attempt variable replacement

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,7 @@ use std::path::PathBuf;
 use std::fs::File;
 use rope::Rope;
 use std::io::BufReader;
+use std::collections::HashMap;
 
 fn main() {
 	let path = PathBuf::new("C:/Rust/helloworld.rs");
@@ -37,19 +38,48 @@ fn main() {
 
 	let mut analysis = BufReader::new(File::open(&"C:/Rust/dxr-temp/unknown_crate.csv").unwrap());
 
+	let mut var_map = HashMap::new();
+	let mut var_ref_map = HashMap::new();
 	for line in analysis.lines() {
 		let mut rdr = csv::Reader::from_string(line.unwrap()).has_headers(false);
 		for row in rdr.records() {
 			let row = row.unwrap();
+			let mut map_record = HashMap::new();
 			println!("{:?}", row);
+
+			let mut it = row.iter();
+			it.next(); // discard first value
+			while let Some(key) = it.next() {
+				if let Some(val) = it.next() {
+					// has pair of values as expected
+					map_record.insert(key.clone(), val.clone());
+				} else {
+					break;
+				}
+			}
+
 			match row[0].as_slice() {
 				"crate" => {},
 				"external_crate" => {},
 				"end_external_crates" => {},
 				"function" => {},
 				"function_ref" => {},
-				"variable" => {},
-				"variable_ref" => {},
+				"variable" => {
+					let key = map_record.get("id").unwrap().clone();
+					var_map.insert(key, map_record);
+				},
+				"var_ref" => {
+					let key = map_record.get("refid").unwrap().clone();
+
+					if !var_ref_map.contains_key("refid") {
+						let v = vec![map_record];
+						var_ref_map.insert(key, v);
+					} else {
+						let vec = var_ref_map.get_mut(&key);
+						vec.unwrap().push(map_record);
+					
+					}
+				},
 				"type" => {},
 				"type_ref" => {},
 				"module" => {},
@@ -62,10 +92,12 @@ fn main() {
 		
 	}
 
-	r.src_remove(5, 10);
-	println!("{}", &r.to_string()[0..20]);
-	r.src_insert(5, "boo".to_string());
-	println!("{}", &r.to_string()[0..20]);
-	r.src_remove(5, 10);
-	println!("{}", &r.to_string()[0..20]);
+
+	for (key, value) in var_map.iter() {
+		println!("{}: \"{}\"", *key, value.get("id").unwrap());
+	}
+
+	for (key, value) in var_ref_map.iter() {
+		println!("{}: \"{:?}\"", *key, value);
+	}
 }
