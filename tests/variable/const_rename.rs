d6446b8b681a33cbbb6f5a8f97d06738272aad6a
Garming <garming_sam@outlook.com>
2015-05-12 23:35:56 +1200

    Test: Const renaming

---
+++ tests/variable/const_rename.rs
@@ -0,0 +1,5 @@
+fn main() {
+    const FOO: u32 = 10;
+    let bar = 2 * FOO;
+    println!("{}", bar);
+}
---
+++ tests/variable/const_rename_out.rs
@@ -0,0 +1,5 @@
+fn main() {
+    const BAZ: u32 = 10;
+    let bar = 2 * BAZ;
+    println!("{}", bar);
+}
