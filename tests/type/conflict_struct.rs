8b9f60e869845b5c7f20c07fdcd95e712dc4635c
Garming Sam <garming_sam@outlook.com>
2015-06-05 04:45:24 +1200

    New conflict struct test

---
+++ tests/type/conflict_struct.rs
@@ -0,0 +1,22 @@
+struct Point {
+    x: i32,
+    y: i32
+}
+
+struct P {
+    x: i32,
+    y: i32
+}
+
+impl Point {
+    fn new(x: i32, y: i32) -> Point {
+        Point {
+            x: x,
+            y: y
+        }
+    }
+}
+
+fn main() {
+    let p: Point = Point::new(1, 1);
+}
