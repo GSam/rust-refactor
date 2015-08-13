be1d2ab4dab1d3d5dd77bbda8fd67ee7e124541f
Garming Sam <garming_sam@outlook.com>
2015-08-13 23:00:03 +1200

    Add new files for field test

---
+++ tests/field/simple_field.rs
@@ -0,0 +1,9 @@
+struct Point {
+    x: u32,
+    y: u32,
+}
+
+fn main() {
+    let x = Point{ x: 2, y: 3 };
+    let _ = x.x;
+}
---
+++ tests/field/simple_field_out.rs
@@ -0,0 +1,9 @@
+struct Point {
+    z: u32,
+    y: u32,
+}
+
+fn main() {
+    let x = Point{ z: 2, y: 3 };
+    let _ = x.z;
+}
